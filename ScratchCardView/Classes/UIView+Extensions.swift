515fa6eebd9c0a26a19cacdc6e32e572c5d1cfaa
Piotr Gorzelany <piotr.gorzelany@gmail.com>
2017-11-26 20:38:29 +0100

    Added methods for getting the view snapshot as a CGImage

--- ScratchCardView/Classes/UIView+Extensions.swift
+++ ScratchCardView/Classes/UIView+Extensions.swift
@@ -15,5 +15,18 @@ extension UIView {
         addSubview(subview)
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[subview]-(0)-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["subview": subview]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[subview]-(0)-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["subview": subview]))
-    }    
+    }
+
+    func getSnapshot() -> CGImage? {
+        UIGraphicsBeginImageContext(bounds.size)
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
+        guard let ctx = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        
+        layer.render(in: ctx)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image?.cgImage
+    }
 }
